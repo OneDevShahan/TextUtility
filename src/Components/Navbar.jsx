7264a8e06c439c9b9346dbbdd9e4ccf1cb8388eb
OneDevShahan <onedevshahan@gmail.com>
2024-03-09 23:47:06 +0530

    Removing a tag

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     <div>
       <nav className="fixed-top navbar navbar-expand-lg bg-body-tertiary" data-bs-theme={props.mode === 'Dark'? 'dark': 'light'}>
         <div className="container-fluid">
-          <a className="navbar-brand" href='/'>{props.title}</a>
+          <span className="navbar-brand" href='/'>{props.title}</span>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
